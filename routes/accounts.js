6f9f4a2ad8a4f855f11d9ab3bf89a71b0fdfa67d
jhSeo <sjh7893@gmail.com>
2018-10-26 14:12:18 +0900

    Accounts menu shows only accounts.

--- routes/accounts.js
+++ routes/accounts.js
@@ -5,57 +5,19 @@ var async = require('async');
 var Web3 = require('web3');
 
 router.get('/:offset?', function(req, res, next) {
-  var config = req.app.get('config');  
-  var web3 = new Web3();
-  web3.setProvider(config.provider);
-  
-  async.waterfall([
-    function(callback) {
-      web3.parity.listAccounts(20, req.params.offset, function(err, result) {
-        callback(err, result);
-      });
-    }, function(accounts, callback) {
-      
-      var data = {};
-      
-      if (!accounts) {
-        return callback({name:"FatDBDisabled", message: "Parity FatDB system is not enabled. Please restart Parity with the --fat-db=on parameter."});
-      }
-      
-      if (accounts.length === 0) {
-        return callback({name:"NoAccountsFound", message: "Chain contains no accounts."});
-      }
-      
-      var lastAccount = accounts[accounts.length - 1];
-      
-      async.eachSeries(accounts, function(account, eachCallback) {
-        web3.eth.getCode(account, function(err, code) {
-          if (err) {
-            return eachCallback(err);
-          }
-          data[account] = {};
-          data[account].address = account;
-          data[account].type = code.length > 2 ? "Contract" : "Account";
-          
-          web3.eth.getBalance(account, function(err, balance) {
-            if (err) {
-              return eachCallback(err);
-            }
-            data[account].balance = balance;
-            eachCallback();
-          });
-        });
-      }, function(err) {
-        callback(err, data, lastAccount);
-      });
-    }
-  ], function(err, accounts, lastAccount) {
-    if (err) {
-      return next(err);
-    }
-    
-    res.render("accounts", { accounts: accounts, lastAccount: lastAccount });
+  var db = req.app.get('eventdb');
+
+  console.log("req.params.offset: " + req.params.offset)
+  if (!req.params.offset) {
+    req.params.offset = 0;
+  } else {
+    req.params.offset = parseInt(req.params.offset);
+  }
+
+  db.find({ balance: { $exists: true }, type: "account" }).sort({ _id: 1 }).skip(req.params.offset).limit(50).exec(function(err, accounts) {
+    res.render('accounts', {accounts: accounts, offset: req.params.offset, stepSize: 50 });
   });
+
 });
 
 module.exports = router;
